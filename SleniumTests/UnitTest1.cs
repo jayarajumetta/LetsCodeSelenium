3456158353ad69fe4ff2bd670a140c1f1d5abb5c
Jayaraju Metta <jayarajumetta@gmail.com>
2019-12-16 00:57:05 +0530

    Selenium C# script to run Azure pipeline

---
+++ SleniumTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SleniumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void IntiateBrowser()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            //chromeOptions.AddArgument("headless");
+            driver = driver = new ChromeDriver(chromeOptions);
+        }
+
+        [TestMethod]
+        public void NaviagteToURL()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+
+        [TestCleanup]
+        public void CloseAllBrowsers()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
